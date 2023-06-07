01b2787367d9c5776d0e48a54479b80bab9e5b75
Chitipat Marsri <116041516+Pat-chiti@users.noreply.github.com>
2023-06-07 15:00:03 +1000

    Update usersPop.sql

--- mysqlfile/usersPop.sql
+++ mysqlfile/usersPop.sql
@@ -5,10 +5,11 @@ username: jim.doe@example.com 				password:jim
 username: sarah.doe@example.com 			password:sarah
 username: mike.doe@example.com 				password:mike
 */
-INSERT INTO Users (email, password, first_name, last_name, DoB) VALUES 
-('john.doe@example.com', '$2y$10$6x/FtOU8ooOObucIoVBXQuGLlcO3UrW7ig7ioPSemgv2TblevRX4e', 'John', 'Doe', '1990-05-20'),
-('jane.doe@example.com', '$2y$10$Fev5NRo4jUQwJycHMHnce.GQlpN0khv0vbN3WPGuQzu5xm0bfGMIm', 'Jane', 'Doe', '1985-12-03'),
-('jim.smith@example.com', '$2y$10$2aaY1C22wBb54nNUyOO3YeFkzTzdk3.AEf/DprtPWKKYHj6daTcvq', 'Jim', 'Smith', '1995-02-15'),
-('sarah.jones@example.com', '$2y$10$OZhW1x4T6pOLJxXj4jW6aeomzpQSl.Dv/cI2rI1309utrkyr4QXJC', 'Sarah', 'Jones', '1988-08-07'),
-('mike.jackson@example.com', '$2y$10$tW9tPa2V8iz7tEUbgpEBFuWNxsGnQUER0AxLEuovHCRGX.xuGnPue', 'Mike', 'Jackson', '1992-11-29');
+INSERT INTO Users (id, email, password, first_name, last_name, DoB) VALUES 
+(1,'john.doe@example.com', '$2y$10$6x/FtOU8ooOObucIoVBXQuGLlcO3UrW7ig7ioPSemgv2TblevRX4e', 'John', 'Doe', '1990-05-20'),
+(2, 'jane.doe@example.com', '$2y$10$Fev5NRo4jUQwJycHMHnce.GQlpN0khv0vbN3WPGuQzu5xm0bfGMIm', 'Jane', 'Doe', '1985-12-03'),
+(3, 'jim.smith@example.com', '$2y$10$2aaY1C22wBb54nNUyOO3YeFkzTzdk3.AEf/DprtPWKKYHj6daTcvq', 'Jim', 'Smith', '1995-02-15'),
+(4, 'sarah.jones@example.com', '$2y$10$OZhW1x4T6pOLJxXj4jW6aeomzpQSl.Dv/cI2rI1309utrkyr4QXJC', 'Sarah', 'Jones', '1988-08-07'),
+(5, 'mike.jackson@example.com', '$2y$10$tW9tPa2V8iz7tEUbgpEBFuWNxsGnQUER0AxLEuovHCRGX.xuGnPue', 'Mike', 'Jackson', '1992-11-29');
+
 
