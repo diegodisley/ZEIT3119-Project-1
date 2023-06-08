886351ff425936175240f9434239fc7132cb9b1d
Chitipat Marsri <116041516+Pat-chiti@users.noreply.github.com>
2023-06-08 19:14:34 +1000

    Add files via upload

---
+++ Project2/database/migrations/2023_05_30_063242_create_transactions_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->string('invoice_number', 50)->unique();
+            $table->primary('invoice_number');
+            $table->unsignedBigInteger('user_id');
+            $table->date('date');
+            $table->double('amount');
+            $table->String('category');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
